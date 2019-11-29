2bb46620cfb48254939d19a04b691c4e38cb6352
Antoine Palazzolo <antoine.palazzolo@gmail.com>
2019-11-29 18:35:37 +0100

    fix compiler error in (empty) tests

--- Tests/ReceiptDecoderTests/ReceiptDecoderTests.swift
+++ Tests/ReceiptDecoderTests/ReceiptDecoderTests.swift
@@ -6,7 +6,6 @@ final class ReceiptDecoderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ReceiptDecoder().text, "Hello, World!")
     }
 
     static var allTests = [
